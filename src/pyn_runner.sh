acc0bcb538558827f575d1afb6a673e7ebd32a9b
WilliamFreedman <freedmanw@bxscience.edu>
2024-05-21 22:01:24 -0400

    added old files and fixed a big with printing bools

---
+++ src/pyn_runner.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Ensure at least one argument is passed
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 [-a|-s|-l|-r] <filename>"
+    exit 1
+fi
+
+OPTSTRING="aslr"
+
+mode_arg=""
+to_run=1
+
+# Process options
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    a)
+      echo "Printing AST"
+      mode_arg="-a"
+      to_run=0
+      ;;
+    s)
+      echo "Printing SAST"
+      mode_arg="-s"
+      to_run=0
+      ;;
+    l)
+      echo "Executing intermediate code"
+      mode_arg="-l"
+      to_run=0
+      ;;
+    r)
+      echo "Executing program"
+      mode_arg=""
+      ;;
+    *)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+# Shift the processed options away to get the filename
+shift $((OPTIND - 1))
+
+# Ensure a filename is provided
+if [ -z "$1" ]; then
+    echo "Error: No filename provided."
+    exit 1
+else
+    filename="$1"
+fi
+
+# Filename for the preprocessed output
+modified_filename="preprocessed.pyn"
+
+# Preprocess the file
+ocamlc -o preprocessor str.cma preprocessor.ml
+
+# Remove intermediate OCaml compilation files
+rm preprocessor.cmo preprocessor.cmi
+
+# Run the preprocessor
+./preprocessor "$filename" "$modified_filename"
+
+# Build the main program
+ocamlbuild -r -pkgs llvm pyn.native
+
+# Execute the appropriate actions based on the mode
+if [ "$to_run" -eq 1 ]; then
+    echo "Executing LLVM code"
+    cat "$modified_filename" | ./pyn.native -l > "$modified_filename.ll"
+    lli "$modified_filename.ll"
+else
+    cat "$modified_filename" | ./pyn.native $mode_arg
+fi
+
+# Optionally, remove temporary files
+# rm "$modified_filename.ll"
+# rm pyn.native
---
+++ src/tester.py
@@ -0,0 +1,54 @@
+import subprocess
+import filecmp
+
+def tester (input,  output):
+    execut = []
+    executable = "./pyn_runner.sh"
+    execut.append(executable)
+    execut.append(input)
+    args = input
+    result = subprocess.run(execut, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text = True, timeout = 5)
+
+    result_str = result.stdout.strip()
+    res_comp = open(output, 'r')
+    res_comp_str = res_comp.read()
+
+    if (result_str != res_comp_str):
+        message = result_str
+        return False, message
+    else:
+        return True, ""
+    
+
+def main ():
+    test_files = [
+        ("../test/print_test.pyn", "../test/print_test.txt"),
+        ("../test/test_basic_if.pyn", "../test/test_basic_if.txt"),
+        ("../test/test_basic_if_else.pyn", "../test/test_basic_if_else.txt"),
+        ("../test/test_func1.pyn", "../test/test_func1.txt"),
+        ("../test/basic_loop_conditional.pyn", "../test/basic_loop_conditional.txt"),
+        ("../test/bool_not_work.pyn", "../test/bool_not_work.txt"),
+        ("../test/bool_test2.pyn", "../test/bool_test2.txt"),
+        ("../test/bool_test3.pyn", "../test/bool_test3.txt"),
+        ("../test/test_break_continue.pyn", "../test/test_break_continue.txt"),
+        ("../test/test_if1.pyn", "../test/test_if1.txt"),
+        ("../test/test_break_loop1.pyn", "../test/test_break_loop1.txt"),
+        ("../test/test_recursion1.pyn", "../test/test_recursion1.txt"),
+        ("../test/test_macro.pyn", "../test/test_macro.txt"),
+         ("../test/test_ops.pyn", "../test/test_ops.txt")
+        
+    ]
+    tests_passed = 0
+    for in_file, out_file in test_files:
+        result, message = tester(in_file, out_file)
+        if result:
+            tests_passed += 1
+            print ("Test Passes! for file: " + in_file)
+        else:
+            print("Test failed for: " + in_file)
+            print(message)
+        print("")
+    print(str(tests_passed) + " out of " + str(len(test_files)) + " tests passed.")
+
+if __name__ == "__main__":
+    main()
